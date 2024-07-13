ce71ff1717fb3235c22db9fe90647870a5216631
vishwas babar <vishwasbabar886@gmail.com>
2024-07-13 20:34:08 +0530

    added signup and login route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,128 @@
 import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { decode, sign, verify, jwt } from 'hono/jwt';
+import { setCookie } from "hono/cookie";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.json({ name: "vishwas vishnu babar" })
+app.get('/', async (c) => {
+  return c.json({ message: 'home route' })
 })
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('this is the signup route')
+app.post('/api/v1/signup', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+  await prisma.$connect()
+
+  interface signupUser {
+    email: string,
+    password: string,
+    name?: string
+  }
+
+  const body: signupUser | any = await c.req.json() // we get the body from this
+
+  const createdUser = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+      name: body.name
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true
+    }
+  })
+
+  if (!createdUser) {
+    c.status(500)
+    return c.json({ message: "internal server error" })
+  }
+
+  if (createdUser) {
+    const token = await sign(createdUser, c.env.JWT_SECRET)
+
+    setCookie(c, 'token', token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 60 * 60 * 24 * 7, // 7 days expiry
+    })
+
+    return c.json({ message: 'signup  successfull!', token })
+  }
+
 })
 
-app.post('/api/v1/login', (c) => {
-  return c.text('this is the login route')
+app.post('/api/v1/login', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body: any = c.req.json();
+
+  // first check the email exist or not
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      email: body.email
+    },
+    select: {
+      email: true,
+      password: true,
+      name: true,
+      id: true,
+    }
+  })
+
+  if (!foundUser) {
+    c.status(404)
+    return c.json({ message: "user with this email id not exist!" })
+  }
+
+  // check the provided password for the provided email id 
+  const isPasswordMatch: boolean = foundUser.password === body.password
+
+  if (isPasswordMatch) {
+    const token = await sign({
+      name: foundUser.name,
+      email: foundUser.email,
+      id: foundUser.id,
+    }, c.env.JWT_SECRET)
+
+    setCookie(c, 'token', token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 60 * 60 * 24 * 7, // 7 days expiry
+    })
+
+    c.status(202)
+    return c.json({ message: 'login successfull!', token })
+  } else {
+    c.status(401)
+    return c.json({ message: "wrong password" })
+  }
+
 })
 
-app.post('/api/v1/blog', (c) => {
+app.post('/api/v1/post-blog', (c) => {
   return c.text('this is the blog route')
 })
 
-app.put('/api/v1/blog', (c) => {
+app.put('/api/v1/update-blog/:id', (c) => {
   return c.text('this is the blog put route')
 })
 
-app.get('/api/v1/blog', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('this is get blog route')
 })
 
 export default app
- 
